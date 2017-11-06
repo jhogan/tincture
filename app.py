80f5eb433381aaa5750ee623f6246c2a45547dec
Jesse Hogan <jessehogan0@gmail.com>
2017-11-06 06:52:30 +0000

    Add .gitignore

--- app.py
+++ app.py
@@ -11,25 +11,20 @@ def app(env, sres):
     reqbody = env['wsgi.input'].read(reqsize).decode('utf-8')
     reqdata = json.loads(reqbody)
 
-    cls  = reduce(getattr, reqdata['class'].split('.'), sys.modules[__name__])
-    meth = reqdata['method']
+    cls  = reduce(getattr, reqdata['_class'].split('.'), sys.modules[__name__])
+    meth = reqdata['_method']
 
-    getattr(cls, meth)(reqdata)
-
-    data = [
-        {'id': 1, 'name': 'Jesse'},
-        {'id': 2, 'name': 'Delia'},
-    ]
+    data = getattr(cls, meth)(reqdata)
 
     data = json.dumps(data)
     data = bytes(data, 'utf-8')
 
-    resheaders=[
+    resheads=[
         ('Content-type', 'text/plain'),
         ('Content-length', str(len(data))),
     ]
 
-    sres('200 OK', resheaders)
+    sres('200 OK', resheads)
     return iter([data])
 
 
