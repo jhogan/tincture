794ec9fcc3ffb66587033383543eda747b1a4992
Jesse Hogan <jessehogan0@gmail.com>
2017-11-07 07:05:09 +0000

    Add exception handling

--- app.py
+++ app.py
@@ -1,32 +1,77 @@
 import json
 import sys
 from functools import reduce
+import pdb; B=pdb.set_trace
 
 def app(env, sres):
     try:
-        reqsize = int(env.get('CONTENT_LENGTH', 0))
-    except(ValueError):
-        reqsize = 0
+        try:
+            reqsize = int(env.get('CONTENT_LENGTH', 0))
+        except(ValueError):
+            reqsize = 0
 
-    reqbody = env['wsgi.input'].read(reqsize).decode('utf-8')
-    reqdata = json.loads(reqbody)
+        reqbody = env['wsgi.input'].read(reqsize).decode('utf-8')
+        reqdata = json.loads(reqbody)
 
-    cls  = reduce(getattr, reqdata['_class'].split('.'), sys.modules[__name__])
-    meth = reqdata['_method']
 
-    data = getattr(cls, meth)(reqdata)
+        try:
+            cls = reqdata['_class']
+        except KeyError:
+            msg = 'The class value was not supplied. Check input.'
+            raise ValueError(msg)
 
-    data = json.dumps(data)
-    data = bytes(data, 'utf-8')
+        classes = ['user']
 
-    resheads=[
-        ('Content-type', 'text/plain'),
-        ('Content-length', str(len(data))),
-    ]
+        if cls not in classes:
+            raise http404('Invalid class')
 
-    sres('200 OK', resheads)
-    return iter([data])
+        cls  = reduce(getattr, cls.split('.'), sys.modules[__name__])
 
+        try:
+            meth = reqdata['_method']
+        except KeyError:
+            msg = 'The method value was not supplied. Check input.'
+            raise ValueError(msg)
+        if meth[0] == '_':
+            raise http404('Invalid method')
+
+        data = getattr(cls, meth)(reqdata)
+
+
+    except Exception as ex:
+        if isinstance(ex, httperror):
+            statuscode = ex.statuscode
+        else:
+            statuscode = '500 Internal Server Error'
+
+        data = {'_exception': repr(ex)}
+
+    else:
+        statuscode = '200 OK'
+
+    finally:
+        data = json.dumps(data)
+        data = bytes(data, 'utf-8')
+
+        resheads=[
+            ('Content-length', str(len(data))),
+            ('Content-type', 'application/json')
+        ]
+
+        sres(statuscode, resheads)
+        return iter([data])
+
+class httperror(Exception):
+    def __init__(self, statuscode, msg):
+        self.statuscode = statuscode
+        self.message = msg
+
+    def __repr__(self):
+        return self.message
+
+class http404(httperror):
+    def __init__(self, msg):
+        super().__init__('404 Not Found', msg)
 
 class user:
 
